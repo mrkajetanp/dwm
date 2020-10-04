1a4aba6d1ed3559714977dc7b590399faf5f5b34
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-10-04 13:05:03 +0100

    Window role rule patch

--- config.h
+++ config.h
@@ -50,13 +50,14 @@ static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
++	 *  WM_WINDOW_ROLE(STRING) = role
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor   float x,y,w,h   floatborderpx */
-	{ "Opera",    NULL,     NULL,           1 << 1,         0,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 1,    0,          0,          -1,        -1 },
-	{ "Emacs", NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
-	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1, 100, 100, 50, 50, 4},
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class   role     instance  title           tags mask  isfloating  isterminal  noswallow  monitor   float x,y,w,h   floatborderpx */
+	{ "Opera", NULL,    NULL,     NULL,           1 << 1,         0,          0,           0,        -1 },
+	{ "Firefox", NULL, NULL,     NULL,           1 << 1,    0,          0,          -1,        -1 },
+	{ "Emacs", NULL, NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
+	{ "st-256color", NULL,      NULL,     NULL,           0,         0,          1,           0,        -1, 100, 100, 50, 50, 4},
+	{ NULL, NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
